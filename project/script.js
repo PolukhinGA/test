b3562585a5398f90c701fe7d96a1c59645080b40
PolukhinGA <polukhinweb@yandex.ru>
2022-04-02 09:56:03 +0300

    second commit

--- project/script.js
+++ project/script.js
@@ -6,5 +6,14 @@ let incr = 10,
 //++incr;
 //--decr;
 
-console.log(incr++);
-console.log(decr--);
+console.log(++incr);
+console.log(--decr);
+
+console.log(5 % 2);
+
+console.log(2 * 4 === 8);
+
+const isChecket = false;
+const isClose = false;
+
+console.log(isChecket || !isClose);
